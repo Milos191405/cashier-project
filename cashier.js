0a5e33affde64324f2bf32e33a6f89f5aa49c360
James Newton <blackslate@lexogram.com>
2024-02-13 22:45:23 +0300

    Converted to using state, switch and Promises
    class Basket
    ---
    * addItem()
      Modified to store just the drink id and the
      total number of drinks with that id. The
      `product` object itself is not stored.
    * displayBasket()
      + Rename parameter to applyDiscount
      + Calculate discountFactor
      + Retrieve the product object to get its price
      + Calculate price in DRY manner, using discountFactor
    
    Products
    ---
    * Simplified the creation of the products array,
      avoiding creating interim variables
    
    basket, Welcome question and recursive orderDrink
    ===
    Break this functionality into five parts:
    
    1. A state object containing the data required to
       conduct the conversation:
       { basket: <Basket instance>,
         action: <"greetCustomer"|"orderDrink"
                  "getQuantity"|"anythingElse"|
                  "checkForDiscount>,
         drink: <current Product instance>,
         more: <true if user wants an additional order>,
         applyDiscount: <true if use has membership card>
       }
    2. A takeOrder() function runs a `while` loop
       until state.action is set to a falsy value
       (empty string), at which point it calculates
       the bill (if appropriate) and says goodbye.
    3. Three utility functions...
       * answerIsAffirmative
       * inputIsValidInteger
       * getProductById
       ... two of which are called from two different
       places, so the code is kept DRY.
    4. A set of async functions which return a promise
       in response to a question set using readline-sync
    5. An async askQuestion() function which awaits
       the new value of state returned by each of the
       questions.
    
    This solution is more complicated than your
    original recursive solution, but it demonstrates a
    principle which you will be using a lot later.
    
    It also allows you to divide the code into distinct
    chunks, with each chunk dealing with a specific
    question.
    
    Add .vscode/launch.json
    ---
    * This allows you to use the debugger
    * Note the last line in particular:
            "console": "integratedTerminal"
      This makes it possible to use readline-sync and
      the debugger at the same time.

--- cashier.js
+++ cashier.js
@@ -1,9 +1,18 @@
+/**
+ * cashier.js
+ */
+
+
+
 const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
+// CLASSES
+
+// Product
 class Product {
   constructor(id, name, price) {
     this.id = id;
@@ -15,55 +24,29 @@ class Product {
     console.log(`${this.id}. ${this.name} ${this.price}€`);
   }
 }
-const espresso = new Product(1, "Espresso", 1.99);
-const doubleEspresso = new Product(2, "Double Espresso", 2.99);
-const americano = new Product(3, "Americano", 3.59);
-const latte = new Product(4, "Latte", 3.9);
-const cappuccino = new Product(5, "Cappuccino", 4.5);
-const macchiato = new Product(6, "Macchiato", 2.69);
-const mocha = new Product(7, "Mocha", 3.5);
-const flatWhite = new Product(8, "Flatwhite", 4.99);
-const mocha2 = new Product(9, "Mocha2", 5.99);
-const dopio = new Product(10, "Dopio", 3.19);
-
-const products = [
-  espresso,
-  doubleEspresso,
-  americano,
-  latte,
-  cappuccino,
-  macchiato,
-  mocha,
-  flatWhite,
-  mocha2,
-  dopio,
-];
 
 // Basket
 class Basket {
   constructor() {
-    this.items = {};
+    this.items = {}; // { <id drink>: <integer quantity> }
   }
 
-  addItem(id, item) {
-    if (this.items[id]) {
-      this.items[id].quantity += item.quantity;
-    } else {
-      this.items[id] = item;
-    }
+  addItem(id, newOrderQuantity) {
+    const currentQuantity = this.items[id] || 0
+    this.items[id] = currentQuantity + newOrderQuantity
   }
 
-  displayBasket(discount) {
+  displayBasket(applyDiscount) {
+    const discountFactor = applyDiscount ? "0.9" : 1
     let totalItems = 0;
     let totalPrice = 0;
 
-    Object.values(this.items).forEach((item) => {
-      totalItems += item.quantity;
-      if (discount === "y") {
-        totalPrice += item.price * item.quantity * 0.9;
-      } else {
-        totalPrice += item.price * item.quantity;
-      }
+
+    Object.entries(this.items).forEach(([ id, quantity ]) => {
+      totalItems += quantity;
+      const product = getProductById(id)
+
+      totalPrice += product.price * quantity * discountFactor
     });
 
     console.log(`Total items ordered: ${totalItems}`);
@@ -71,78 +54,264 @@ class Basket {
   }
 }
 
-const basket = new Basket();
+// PRODUCTS
 
-//Welcome
-rl.question(
-  "Welcome to the coffee shop! Would you like to order? ",
-  function (answer) {
-    if (answer.toLowerCase() === "y") {
-      console.log(
-        "Great! Have you already decided? Here is our offer to see one more time!"
-      );
-      products.forEach((product) => product.display());
-      orderDrink();
-    } else {
-      console.log("Thank you! See you next time!");
-      rl.close();
-    }
+// const espresso = new Product(1, "Espresso", 1.99);
+// const doubleEspresso = new Product(2, "Double Espresso", 2.99);
+// const americano = new Product(3, "Americano", 3.59);
+// const latte = new Product(4, "Latte", 3.9);
+// const cappuccino = new Product(5, "Cappuccino", 4.5);
+// const macchiato = new Product(6, "Macchiato", 2.69);
+// const mocha = new Product(7, "Mocha", 3.5);
+// const flatWhite = new Product(8, "Flatwhite", 4.99);
+// const mocha2 = new Product(9, "Mocha2", 5.99);
+// const dopio = new Product(10, "Dopio", 3.19);
+
+// const products = [
+//   espresso,
+//   doubleEspresso,
+//   americano,
+//   latte,
+//   cappuccino,
+//   macchiato,
+//   mocha,
+//   flatWhite,
+//   mocha2,
+//   dopio,
+// ];
+
+// You can create the products array directly from the instances
+// of the Product class.
+
+// I made the prices the same as the product ids, to make it
+// easier to check if the total price is correctly calculated.
+const products = [
+  new Product(1,  "Espresso",        1),
+  new Product(2,  "Double Espresso", 2),
+  new Product(3,  "Americano",       3),
+  new Product(4,  "Latte",           4),
+  new Product(5,  "Cappuccino",      5),
+  new Product(6,  "Macchiato",       6),
+  new Product(7,  "Mocha",           7),
+  new Product(8,  "Flatwhite",       8),
+  new Product(9,  "Mocha2",          9),
+  new Product(10, "Dopio",          10),
+];
+
+
+// An "Immediately Invoked Function Expression" (IIFE) will run
+// automatically. You don't need to call...
+//
+//   takeOrder()
+//
+// ... to make it run.
+
+;(async function takeOrder() {
+  const basket = new Basket()
+  let state =  { action: "greetCustomer", basket }
+  // { action, basket, drink, more, applyDiscount}
+  // basket.items will be { <drink id>: <quantity>, ... }
+  // action will be set to "" when the order is complete
+
+  while (state.action) {
+    state = await askQuestion(state)
   }
-);
+
+  if (state.drink) { //
+    basket.displayBasket(state.applyDiscount)
+  }
+
+  console.log("Thank you! See you next time!");
+  rl.close();
+})()
+
+
+// Switch statement to call a different function for each
+// action state of the conversation
+async function askQuestion(state) {
+  switch (state.action) {
+    case "greetCustomer":
+      return await greetCustomer(state)
+    case "orderDrink":
+      return await orderDrink(state)
+    case "getQuantity":
+      return await getQuantity(state)
+    case "anythingElse":
+      return await anythingElse(state)
+    case "checkForDiscount":
+      return await checkForDiscount(state)
+    }
+}
+
+
+// Utilities
+
+// Define affirmativeAnswers once, so it is not recreated each
+// time answerIsAffirmative() is called
+const affirmativeAnswers = ["y", "yes", "yeah", "sure", "ok"]
+
+function answerIsAffirmative(answer) {
+  // Convert to lower case and remove any punctuation, so
+  // "Sure!" will be converted to "sure"
+  answer = answer.toLowerCase().replace(/[.?!]/, "")
+  return affirmativeAnswers.indexOf(answer) > -1
+}
+
+
+
+function inputIsValidInteger(input, min, max) {
+  input = Number(input)
+  const isValid = !isNaN(input)
+                && input >= min
+                && input <= max
+                && !(input % 1) // no floating point numbers
+  return isValid && input // false or integer (which may be 0)
+}
+
+
+
+function getProductById(id) {
+  return products.find( product => (
+    product.id === Number(id)
+  ));
+}
+
+
+
+// The rl function expects a callback, so it's not possible to
+// return a value directly from the conversation function calls.
+// Each of the functions below:
+// * Is asynchronous
+// * Returns a Promise
+// * Resolves the Promise with a state object
+// * May update the value of the `action` property of the state
+//   object, if the conversation is to move to a different topic.
+
+// Welcome
+async function greetCustomer(state) {
+  return new Promise( resolve => {
+    rl.question(
+      "Welcome to the coffee shop! Would you like to order? (y/n) ",
+      treatAnswerToGreeting
+    )
+
+    function treatAnswerToGreeting(answer) {
+      if (answerIsAffirmative(answer)) {
+        console.log(
+          "Great! Have you already decided? Here is our offer to see one more time!"
+        );
+        products.forEach((product) => product.display());
+        resolve({ ...state, action: "orderDrink" })
+
+      } else { // No order
+        resolve({}) // no action, basket is not used
+      }
+    }
+  })
+}
+
+
 
 // Order
-function orderDrink() {
-  rl.question("What would you like to drink? (Enter ID) ", function (id) {
-    const drink = products.find((product) => product.id === parseInt(id));
-    if (drink) {
-      // quantity
+async function orderDrink(state) {
+  const { more } = state
 
-      rl.question(
-        `How many ${drink.name} would you like? `,
-        function (quantity) {
-          if (!isNaN(quantity) && parseInt(quantity) > 0) {
-            drink.quantity = parseInt(quantity);
-            basket.addItem(id, drink);
-            console.log(`${quantity} ${drink.name}  added to your basket.`);
-          } else {
-            console.log(
-              "Invalid quantity. Please enter a valid number greater than 0."
-            );
-          }
+  return new Promise( resolve => {
+    rl.question(
+      `What ${more ? "else " : ""}would you like to drink? (Enter ID) `,
+      chooseDrink
+    )
 
-          // Order more
+    function chooseDrink(id) {
+      const drink = getProductById(id) // undefined if id invalid
 
-          rl.question(
-            "Would you like to order anything else? (yes/no) ",
-            function (additionalOrder) {
-              if (additionalOrder.toLowerCase() === "y") {
-                orderDrink();
-              } else {
-                // Membership card
+      if (drink) {
+        const action = id ? "getQuantity" : ""
+        resolve({ ...state, drink, action })
 
-                rl.question(
-                  "Do you have our membership card? ",
-                  function (membership) {
-                    if (membership.toLowerCase() === "y") {
-                      console.log(`You have a 10% discount.`);
-                    }
-                    if (membership.toLowerCase() === "y") {
-                      basket.displayBasket("y");
-                    } else {
-                      basket.displayBasket();
-                    }
-                    rl.close();
-                    console.log("Thank you! See you next time!");
-                  }
-                );
-              }
-            }
-          );
-        }
-      );
-    } else {
-      console.log("Invalid drink ID. Please try again.");
-      orderDrink();
+      } else {
+        console.log("I'm sorry, we don't have that on the menu.")
+        resolve (state) // unchanged. action = "orderDrink"
+      }
     }
-  });
+  })
+}
+
+
+
+// Number of drinks
+async function getQuantity(state) {
+  const { drink } = state
+  const { id } = drink
+
+  return new Promise( resolve => {
+    rl.question(
+      `How many ${drink.name} would you like? `,
+      checkQuantity
+    )
+
+    function checkQuantity(quantity) {
+      quantity = inputIsValidInteger(quantity, 1, 100)
+
+      if (typeof quantity === "number") {
+        const { basket } = state
+
+        basket.addItem(id, quantity);
+        console.log(`${quantity} ${drink.name} added to your basket.`);
+        resolve({ ...state, action: "anythingElse" })
+
+      } else {
+        console.log(
+          "Invalid quantity. Please enter a number greater than 0."
+        );
+
+        resolve( state )
+        // action is still "getQuantity"
+        // drink and basket are unchanged
+      }
+    }
+  })
+}
+
+
+
+// Order more
+async function anythingElse(state) {
+  return new Promise ( resolve => {
+    rl.question(
+      "Would you like to order anything else? (y/n) ",
+      additionalOrder
+    )
+
+    function additionalOrder(answer) {
+      if (answerIsAffirmative(answer)) {
+        resolve({ ...state, more: true, action: "orderDrink" })
+
+      } else {
+        resolve({ ...state, action: "checkForDiscount" })
+      }
+    }
+  })
+}
+
+
+
+// Membership card
+async function checkForDiscount(state) {
+  return new Promise( resolve => {
+    rl.question(
+      "Do you have our membership card? (y/n) ",
+      clientHasMembershipCard
+    )
+
+    function clientHasMembershipCard(membership) {
+      const applyDiscount =  membership.toLowerCase() === "y"
+
+      if (applyDiscount) {
+        console.log(`You have a 10% discount.`);
+      }
+
+      resolve({ ...state, applyDiscount, action: "" })
+    }
+  })
 }
